f7ebb9e70d02b5d90c2da878dedaeb309e9e561c
tahmidmahdi <tahmid5550@gmail.com>
2021-02-18 17:48:35 +0600

    pin checked

---
+++ index.js
@@ -0,0 +1,20 @@
+var price = 11;
+console.log(typeof price);
+var priceType = typeof(price);
+console.log(priceType);
+
+var names = 'ABCDEF'
+console.log(names.toLocaleLowerCase());
+console.log(names.indexOf('D'));
+
+console.log(25 + 'a');
+
+number = '25';
+number =+ number;
+console.log('number', number, typeof number);
+
+
+
+var array = [1,2,3,4,5,6];
+var slice = array.slice(0, 3);
+console.log(slice);
---
+++ projects/pin-matcher-master/test.js
@@ -0,0 +1,7 @@
+var a = 'abcdef';
+// for (let i = 0; i < a.length; i++) {
+//     const element = a[i];
+    
+    
+// }
+console.log(a);
